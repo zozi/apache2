7f33fea6816c6bd894863065dbd2fe46c030912e
Theofilos Papapanagiotou <theofilos@gmail.com>
2013-10-14 23:57:07 +0200

    Style clean-up, single-quote, typo, error handling for mod_pagespeed in Centos.

--- recipes/mod_pagespeed.rb
+++ recipes/mod_pagespeed.rb
@@ -1,23 +1,24 @@
-remote_file "#{Chef::Config[:file_cache_path]}/mod-pagespeed." + node["apache2"]["mod_pagespeed"]["link_extension"] do
-  source node["apache2"]["mod_pagespeed"]["package_link"]
-  mode 0644
-  action :create_if_missing
+if node['apache2']['mod_pagespeed']['link_extension']
+  remote_file "#{Chef::Config[:file_cache_path]}/mod-pagespeed." + node['apache2']['mod_pagespeed']['link_extension'] do
+    source node['apache2']['mod_pagespeed']['package_link']
+    mode 0644
+    action :create
+  end
 end
-
 # switch between dpkg&yum, since the package resource needs version number.
-case node["platform_family"]
-when "debian"
-  dpkg_package "mod_pagespeed" do
-    source "#{Chef::Config[:file_cache_path]}/mod-pagespeed." + node["apache2"]["mod_pagespeed"]["link_extension"]
+case node['platform_family']
+when 'debian'
+  dpkg_package 'mod_pagespeed' do
+    source "#{Chef::Config[:file_cache_path]}/mod-pagespeed." + node['apache2']['mod_pagespeed']['link_extension']
     action :install
   end
-when "rhel"
-  yum_package "mod_pagespeed" do
-    source "#{Chef::Config[:file_cache_path]}/mod-pagespeed." + node["apache2"]["mod_pagespeed"]["link_extension"]
+when 'rhel'
+  yum_package 'mod_pagespeed' do
+    source "#{Chef::Config[:file_cache_path]}/mod-pagespeed." + node['apache2']['mod_pagespeed']['link_extension']
     action :install
   end
 end
 
-apache_module "pagespeed" do
+apache_module 'pagespeed' do
   conf true
 end
